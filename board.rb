464cc418b77bd20e0536da02d16e0814439e0546
App Academy Student <student@appacademy.io>
2017-10-09 17:43:40 -0700

    filled the board with pieces

--- board.rb
+++ board.rb
@@ -1,36 +1,40 @@
 class Board
 
-attr_accessor :grid
+  attr_accessor :grid
 
-def initialize(grid= Array.new(8) {Array.new(8)} )
-  @grid=grid
-  populate
-end
+  def initialize(grid= Array.new(8) {Array.new(8)} )
+    @grid=grid
+    populate
+  end
 
-def populate
-  @grid.each_with_index do |row, i|
-    row.each_index do |j|
-      if i < 2
-        self[[i,j]] = Piece.new
+  def populate
+    @grid.each_with_index do |row, i|
+      row.each_index do |j|
+        if i < 2
+          self[[i,j]] = Piece.new
+        elsif i >1 && i < 6
+          self[[i,j]] =  NullPiece.new
+        else
+          self[[i,j]] = Piece.new
+        end
       end
     end
   end
-end
 
-def [](pos)
-  row, col = pos
-  @grid[row][col]
-end
+  def [](pos)
+    row, col = pos
+    @grid[row][col]
+  end
 
-def []=(pos, piece)
-  row, col =pos
-  @grid[row][col]= piece
-end
+  def []=(pos, piece)
+    row, col =pos
+    @grid[row][col]= piece
+  end
 
-def move_piece(start_pos, end_pos)
-  raise ArgumentError.new("There is nothing at this position") if @grid.start_pos.nil?
-  raise ArgumentError.new("There is nothing at this position") if @grid.end_pos.nil?
-end
+  def move_piece(start_pos, end_pos)
+    raise ArgumentError.new("There is nothing at this position") if self[start_pos].nil?
+    raise ArgumentError.new("There is already a piece at this position") if self[end_pos].nil?
+  end
 
 end
 
